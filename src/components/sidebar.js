77548983f0f72f718ca2ca3b584d3ef801b75069
OmarContreras <recontraoc@gmail.com>
2024-05-31 15:56:43 -0600

    JAvi le toca

--- src/components/sidebar.js
+++
@@ -1,9 +0,0 @@
-const Sidebar = () => {
-    return (
-      <div className="w-24 h-screen bg-gradient-to-t from-blue-500 to-violet-600 flex flex-col">
-      </div>
-    );
-  };
-  
-  export default Sidebar;
-  
