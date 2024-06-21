6c5db719e58f09c0f10c5b1de1038c5495792db9
JavierAcosta07 <a6521150022@utch.edu.mx>
2024-06-20 21:20:35 -0400

    Token page

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,51 +1,64 @@
-'use client';
-import React, { useState } from 'react';
+"use client";
 
+import { useState, useRef } from 'react';
+
+export default () => {
+  const [tokens, setTokens] = useState(['', '', '', '']);
+  const inputsRef = useRef([]);
+
+  const handleChange = (index, value) => {
+    if (value.length > 1) return;
+
+    const newTokens = [...tokens];
+    newTokens[index] = value;
+    setTokens(newTokens);
+
+    if (value && index < 3) {
+      // Move to the next input if current input is filled
+      inputsRef.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (index, event) => {
+    if (event.key === 'Backspace' && tokens[index] === '' && index > 0) {
+      // Move to the previous input if current input is empty and backspace is pressed
+      inputsRef.current[index - 1].focus();
+    }
+  };
 
   return (
     <main className="bg-[#142233] w-full h-screen flex flex-col items-center justify-center px-4">
-      <div className="max-w-sm w-full text-white">
+      <div className="max-w-sm w-full text-gray-600">
         <div className="text-center">
-          <img src="/Logo.png" width={150} className="m-auto" />
-          <h1 className='text-2xl font-bold text-[#6D72F2]'>ChatBot</h1>
-          <div className="mt-4">
-            <h3 className="text-white text-2xl font-bold sm:text-3xl">Log in to your account</h3>
-            <p className="text-sm pt-4">Your credentials are provided by the university, if you do not have yet, communicate with the institution</p>
+          <img src="/Logo.png" width={120} className="mx-auto " />
+          <div className="mt-5 space-y-2">
+            <h3 className="text-[#6D72F2] text-2xl font-bold sm:text-3xl">Enter your Token</h3>
+            <p className="text-white">You received a Token in your email to verify your email</p>
           </div>
         </div>
-        <form  className="mt-8 space-y-5">
-          <div>
-            <label className="font-medium">Email</label>
-            <input
-              type="email"
-              name="email"
-              required
-              className="w-full mt-2 px-3 py-2 text-gray-800 bg-white outline-none border focus:border-[#6D72F2] shadow-sm rounded-lg"
-            />
-          </div>
-          <div>
-            <label className="font-medium">Password</label>
-            <input
-              type="password"
-              name="password"
-              required
-              className="w-full mt-2 px-3 py-2 text-gray-800 bg-white outline-none border focus:border-[#6D72F2] shadow-sm rounded-lg"
-            />
-          </div>
-          {error && <p className="text-red-500">{error}</p>}
-          <button
-            type="submit"
-            className="w-full px-4 py-2 text-white font-medium bg-[#B165BC] hover:bg-[#6D72F2] active:bg-[#B165BC] rounded-lg duration-150"
-          >
-            Sign in
-          </button>
-          <div className="text-center">
-            <a className="hover:text-[#6D72F2]">Forgot password?</a>
-          </div>
-        </form>
+        <div className="flex justify-center items-center mb-4">
+          {tokens.map((token, index) => (
+            <div key={index} className="flex items-center">
+              <input
+                ref={(el) => inputsRef.current[index] = el}
+                value={token}
+                onChange={(e) => handleChange(index, e.target.value)}
+                onKeyDown={(e) => handleKeyDown(index, e)}
+                className="w-12 mx-1 px-3 py-2 text-black bg-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg text-center"
+              />
+              {index < tokens.length - 1 && <span className="text-white mx-1">-</span>}
+            </div>
+          ))}
+        </div>
+        <button
+          className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+        >
+          Enter
+        </button>
+        <div className="text-center mt-4">
+          <a className="text-white hover:text-indigo-600">Didn't receive your Token?</a>
+        </div>
       </div>
     </main>
   );
-
-
-export default LoginForm;
+};
