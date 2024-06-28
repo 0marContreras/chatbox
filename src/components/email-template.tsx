5a842ae2bd523ef6a6887800e0bbc2c5bc1de9d5
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2024-06-28 11:54:13 -0600

    email part 2

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -6,9 +6,11 @@ interface EmailTemplateProps {
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   token,
 }) => (
-  <div className='bg-grey flex justify-between items-center '>
+  <div style={{backgroundColor: '#142233'}}>
+    <center>
     <img src='https://i.imgur.com/1alqVrq.png' width={100} height={100}></img>
-    <h1 className='text-white font-bold text-xl'>Welcome!</h1>
-    <h1 className='text-white font-bold text-xxl'>Your auth token is: {token}</h1>
+    <h1 style={{color: '#6D72F2', fontSize: '60px'}} >Welcome!</h1>
+    <h1 style={{color: 'aliceblue'}} >Your auth token is: {token}</h1>
+    </center>
   </div>
 );
