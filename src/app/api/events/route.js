b985fdd97a654f11886e4d83a5112fd1a3bc69f9
OmarContreras <recontraoc@gmail.com>
2024-07-03 19:01:02 -0600

    DELETE inputs

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -9,3 +9,12 @@ export async function GET(){
     console.log(setting)
     return NextResponse.json(setting, { status: 200 });
 }
+
+export async function DELETE(req){
+    connectToDatabase()
+    const {_id} = await req.json();
+    const setting = await settings.findById(_id)
+    await settings.findByIdAndDelete(_id)
+    console.log(setting)
+    return NextResponse.json(setting, { status: 200 });
+}
