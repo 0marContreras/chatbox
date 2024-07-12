3983429c7a6511f225d7217768c4fb5af94dc8c5
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2024-07-12 13:31:09 -0600

    Rework in additial info collection

--- src/models/additionalInfo.js
+++ src/models/additionalInfo.js
@@ -4,4 +4,6 @@ const additionalInfo = new mongoose.Schema({
     Additional_info: {type: String},
 });
 
+//additionalInfo.set('collection', 'additionalInfo')
+
 export default mongoose.models.additionalInfo || mongoose.model('additionalInfo', additionalInfo);
