dd4446ae31ef7225ea869b6d0091c4ebdea1fb79
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2024-07-12 13:36:19 -0600

    Post

--- src/app/api/info/route.js
+++ src/app/api/info/route.js
@@ -9,3 +9,15 @@ export async function GET(){
     console.log(info)
     return NextResponse.json(info, { status: 200 });
 }
+
+export async function PUT(req){
+    const {_id, newInfo} = await req.json();
+    connectToDatabase()
+    try{
+        const info = await additionalInfo.findByIdAndUpdate(_id, {"Additinal_info": newInfo})
+        return NextResponse.json(info, { status: 200 });
+    }catch(e){
+        return NextResponse.json({"error": "internal error"}, { status: 500 });
+    }
+    
+}
