8395398fd655721da404d538e57097849b864086
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2024-08-07 23:00:21 -0600

    a ver

--- src/components/Textbox.jsx
+++ src/components/Textbox.jsx
@@ -49,7 +49,7 @@ const Textbox = () => {
       }
       const data = await response.json();
       console.log('Respuesta del servidor:', data);
-      await fetchInfo();
+      fetchInfo();
 
       // Mostrar la alerta
       setAlertVisible(true);
