6e12927f8db7034914eff76cad6105db12a09ac0
OmarContreras <recontraoc@gmail.com>
2024-08-08 17:09:30 -0600

    deploy

--- src/components/password-template.tsx
+++ src/components/password-template.tsx
@@ -10,7 +10,7 @@ export const PasswordTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
     <center>
     <img src='https://i.imgur.com/1alqVrq.png' width={100} height={100}></img>
     <h1 style={{color: '#6D72F2', fontSize: '60px'}} >Welcome!</h1>
-    <h1 style={{color: 'aliceblue'}} >Click <a style={{color: '#6D72F2'}} href="http://localhost:3000/passwordreset">here</a> to reset your password</h1>
+    <h1 style={{color: 'aliceblue'}} >Click <a style={{color: '#6D72F2'}} href="https://chatbox.website/passwordreset">here</a> to reset your password</h1>
     </center>
   </div>
 );
